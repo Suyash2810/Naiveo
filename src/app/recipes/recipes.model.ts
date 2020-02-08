b4c83caab5da6447a7a0fac1121bb15550873a4e
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-02-08 16:25:33 +0530

    Created the model for recipes.

---
+++ src/app/recipes/recipes.model.ts
@@ -0,0 +1,7 @@
+export interface Recipe {
+
+    id: string;
+    ingredients: Array<string>;
+    title: string;
+    imagePath: string;
+}
--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Array<Recipe> = [
+    {
+      id: '1',
+      title: "Burger & Fries",
+      imagePath: "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&h=425&w=640",
+      ingredients: [
+        'Bun',
+        'Potatoes',
+        'Salt',
+        'Spices',
+        'Sauce',
+        'Salad'
+      ]
+    },
+    {
+      id: '2',
+      title: "Pizza",
+      imagePath: "https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&h=480&w=640",
+      ingredients: [
+        'Bread Base',
+        'Mozrella',
+        'Salt',
+        'Spices',
+        'Pizza Sauce',
+        'Tomatoes',
+        'Onions'
+      ]
+    }
+  ];
 
   constructor() { }
 
