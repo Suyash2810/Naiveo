d8b32df9829ab062c3958e7d40070f42169bab06
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-02-08 17:00:32 +0530

    Created the service for state management of recipes component.

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipes.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -8,39 +9,13 @@ import { Recipe } from './recipes.model';
 })
 export class RecipesPage implements OnInit {
 
-  recipes: Array<Recipe> = [
-    {
-      id: '1',
-      title: "Burger & Fries",
-      imagePath: "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&h=425&w=640",
-      ingredients: [
-        'Bun',
-        'Potatoes',
-        'Salt',
-        'Spices',
-        'Sauce',
-        'Salad'
-      ]
-    },
-    {
-      id: '2',
-      title: "Pizza",
-      imagePath: "https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&h=480&w=640",
-      ingredients: [
-        'Bread Base',
-        'Mozrella',
-        'Salt',
-        'Spices',
-        'Pizza Sauce',
-        'Tomatoes',
-        'Onions'
-      ]
-    }
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipesService) { }
 
   ngOnInit() {
+
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipes.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RecipesService {
+
+  recipes: Array<Recipe> = [
+    {
+      id: '1',
+      title: "Burger & Fries",
+      imagePath: "https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&h=425&w=640",
+      ingredients: [
+        'Bun',
+        'Potatoes',
+        'Salt',
+        'Spices',
+        'Sauce',
+        'Salad'
+      ]
+    },
+    {
+      id: '2',
+      title: "Pizza",
+      imagePath: "https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&h=480&w=640",
+      ingredients: [
+        'Bread Base',
+        'Mozrella',
+        'Salt',
+        'Spices',
+        'Pizza Sauce',
+        'Tomatoes',
+        'Onions'
+      ]
+    }
+  ];
+
+  constructor() { }
+
+  getRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(id: string) {
+
+    return {
+      ...this.recipes.find(recipe => {
+        recipe.id === id;
+      })
+    };
+  }
+}
