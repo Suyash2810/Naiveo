f4bf32e5dddfc7958a8b766b1a4f03f3c3a6577d
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-02-15 22:00:33 +0530

    Created the places and discover sub page.

---
+++ src/app/places/places-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PlacesPage } from './places.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PlacesPage
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PlacesPageRoutingModule {}
