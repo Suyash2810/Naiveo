92275cdc9db95d2a8fb298299a2673346b149485
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-03-06 19:54:51 +0530

    Added loadController for log in.

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
@@ -9,13 +10,19 @@ import { Router } from '@angular/router';
 })
 export class AuthPage implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private loadingCntrl: LoadingController) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login();
-    this.router.navigateByUrl('/places');
+    this.loadingCntrl.create({ keyboardClose: true, message: 'Loading...' }).then(loadController => {
+      loadController.present();
+      setTimeout(() => {
+        loadController.dismiss()
+        this.router.navigateByUrl('/places');
+      }, 2000);
+    });
   }
 }
