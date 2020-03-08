c78d7a6835f6c6c99631f54feea5dc5cda3fc192
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-03-08 17:37:37 +0530

    Updated the offer item component.

--- src/app/places/offers/offer-item/offer-item.component.ts
+++ src/app/places/offers/offer-item/offer-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Place } from '../../places.model';
 
 @Component({
   selector: 'app-offer-item',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferItemComponent implements OnInit {
 
+  @Input() offer: Place;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  getDate() {
+    return new Date();
+  }
 }
