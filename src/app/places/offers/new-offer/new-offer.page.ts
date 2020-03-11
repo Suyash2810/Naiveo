a2e710204a7eb37230240efc71d6f4e23d432a86
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-03-11 15:42:08 +0530

    new offer page - Created and synced the reactive form.

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewOfferPage implements OnInit {
 
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.form = new FormGroup({
+      title: new FormControl(null, { validators: Validators.required }),
+      description: new FormControl(null, { validators: [Validators.required, Validators.maxLength(180)] }),
+      price: new FormControl(0, { validators: [Validators.required, Validators.minLength(1)] }),
+      dateFrom: new FormControl(null, { validators: [Validators.required] }),
+      dateTill: new FormControl(null, { validators: Validators.required })
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
   }
 
 }
