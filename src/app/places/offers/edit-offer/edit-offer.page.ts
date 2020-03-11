b40ac2b408859cf233a63f8d205732214b0baf23
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-03-11 16:47:59 +0530

    edit offer - created and synced reactive form.

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../../places.model';
+import { PlacesService } from '../../places.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor() { }
+  offer: Place;
+  id: string;
+  form: FormGroup;
+
+  constructor(private placeService: PlacesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = params['offerID'];
+        this.offer = this.placeService.getPlaceById(this.id);
+      }
+    );
+
+    this.form = new FormGroup({
+      title: new FormControl(this.offer.title, { validators: Validators.required }),
+      description: new FormControl(this.offer.description, { validators: Validators.maxLength(180) }),
+      price: new FormControl(this.offer.price, { validators: Validators.required })
+    });
   }
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
