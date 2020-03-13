9d59ed732e53103fa2b43061cf856f60281b4474
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-03-13 19:27:30 +0530

    feat: Added new fields in the places model.

--- src/app/places/places.model.ts
+++ src/app/places/places.model.ts
@@ -4,17 +4,22 @@ export interface Place {
     description: string;
     imageUrl: string;
     price: number;
+    availableFrom: Date;
+    availableTill: Date;
 }
 
 export class Place implements Place {
 
-    id: string; title: string; description: string; imageUrl: string; price: number;
+    id: string; title: string; description: string; imageUrl: string; price: number; availableFrom: Date;
+    availableTill: Date;
 
-    constructor(id, title, description, imageUrl, price) {
+    constructor(id, title, description, imageUrl, price, availableFrom, availableTill) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.imageUrl = imageUrl;
         this.price = price;
+        this.availableFrom = availableFrom;
+        this.availableTill = availableTill;
     }
 }
