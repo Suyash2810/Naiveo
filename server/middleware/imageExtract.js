fa1991868123fc19d4610a1f4e186a4804366faa
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-03-23 21:11:16 +0530

    feat: added multer for image extraction.

---
+++ server/middleware/imageExtract.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+const moment = require('moment');
+
+const MIME_TYPE = {
+    "image/png": "png",
+    "image/jpeg": "jpeg",
+    "image/jpg": "jpg"
+}
+
+const storage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        let error = new Error('MIMETYPE is not valid.');
+        let isValid = MIME_TYPE[file.mimetype];
+        if (isValid) {
+            error = null;
+        }
+
+        callback(error, './server/public/src/images');
+    },
+    filename: (request, file, callback) => {
+
+        let name = file.originalname.split('.').join('_') + "_" + moment.format().toString() + "." + MIME_TYPE[file.mimetype];
+
+        callback(null, name);
+    }
+});
+
+module.exports = multer({
+    storage: storage
+}).single('image');
