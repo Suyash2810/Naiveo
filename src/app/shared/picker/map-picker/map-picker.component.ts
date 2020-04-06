4ab964eb96a7794196b2867af3e45948d4fc0513
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-04-06 18:36:36 +0530

    feat: set up the maps in map picker.

--- src/app/shared/picker/map-picker/map-picker.component.ts
+++ src/app/shared/picker/map-picker/map-picker.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import tt from '@tomtom-international/web-sdk-maps';
 
 @Component({
   selector: 'app-map-picker',
   templateUrl: './map-picker.component.html',
   styleUrls: ['./map-picker.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MapPickerComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    var map = tt.map({
+      key: 'WeYcSynyjv3gezHYOY5wm67ufAeuDgKK',
+      container: 'map',
+      style: 'tomtom://vector/1/basic-main',
+      center: [18.5204, 73.8567]
+    });
+    map.addControl(new tt.NavigationControl());
+    
+  }
 }
