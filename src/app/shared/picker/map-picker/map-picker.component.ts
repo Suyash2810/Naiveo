ac65bd68aa7874613a20822121f63139f482cb7e
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-04-06 16:55:41 +0530

    feat: created map modal and map picker components.

---
+++ src/app/shared/picker/map-picker/map-picker.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-picker',
+  templateUrl: './map-picker.component.html',
+  styleUrls: ['./map-picker.component.scss'],
+})
+export class MapPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
