92e0670a1ce606d91fced39a1a1cd437a797dfdd
suyash2810 <suyash.awasthi28100182@gmail.com>
2020-06-24 19:09:08 +0530

    feat: settings page design#1

--- src/app/user-settings/user-settings-routing.module.ts
+++ src/app/user-settings/user-settings-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserSettingsPage } from './user-settings.page';
+import { LoadingSpinnerModule } from '../shared/loading-spinner/loading-spinner.module';
 
 const routes: Routes = [
   {
@@ -11,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), LoadingSpinnerModule],
   exports: [RouterModule],
 })
-export class UserSettingsPageRoutingModule {}
+export class UserSettingsPageRoutingModule { }
