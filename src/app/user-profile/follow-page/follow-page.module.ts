a9539d47943f256b2fc636d165d6f60817cca0d0
suyash2810 <suyash.awasthi28100182@gmail.com>
2020-06-29 20:54:40 +0530

    feat: displayed following list in connections page.

--- src/app/user-profile/follow-page/follow-page.module.ts
+++ src/app/user-profile/follow-page/follow-page.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { FollowPagePageRoutingModule } from './follow-page-routing.module';
 
 import { FollowPagePage } from './follow-page.page';
+import { LoadingSpinnerModule } from 'src/app/shared/loading-spinner/loading-spinner.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FollowPagePageRoutingModule
+    FollowPagePageRoutingModule,
+    LoadingSpinnerModule
   ],
   declarations: [FollowPagePage]
 })
-export class FollowPagePageModule {}
+export class FollowPagePageModule { }
--- src/app/user-profile/follow-page/follow-page.page.ts
+++ src/app/user-profile/follow-page/follow-page.page.ts
@@ -3,6 +3,7 @@ import { SegmentChangeEventDetail } from "@ionic/core";
 import { UserService } from '../user-profile.service';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-follow-page',
@@ -14,11 +15,34 @@ export class FollowPagePage implements OnInit, OnDestroy {
   userId: string;
   userData: any;
   userSubscription: Subscription;
+  segment: string = "following";
+  isLoading: boolean = true;
 
-  constructor(private profileService: UserService, private authService: AuthService) { }
+  constructor(private profileService: UserService, private authService: AuthService, private alertController: AlertController) { }
 
   ngOnInit() {
     this.userId = this.authService.getUserId();
+    this.userSubscription = this.profileService.fetchUserInfo(this.userId)
+      .subscribe(
+        (response) => {
+          if (response.info.length != 0) {
+            this.userData = response.info[0];
+            console.log(this.userData);
+            this.isLoading = false;
+          } else {
+            this.userData = null;
+          }
+        },
+        async error => {
+          const alert = await this.alertController.create({
+            header: "Error",
+            message: error,
+            buttons: ['Ok']
+          });
+
+          await alert.present();
+        }
+      );
   }
 
 
@@ -27,6 +51,6 @@ export class FollowPagePage implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-
+    this.userSubscription.unsubscribe();
   }
 }
